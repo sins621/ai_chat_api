514857a527d0dc09490df0f3fc7421091763a3df
Sins_621 <sinsmailza@gmail.com>
2025-02-19 20:08:50 +0200

    update/change-to-blueprint

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 import ollama
 
 desiredModel = "qwen2.5:1.5b"
 
 app = Flask(__name__)
+ai_chat_bp = Blueprint("ai", __name__, url_prefix="/api/ai")
 
 
-@app.route("/")
+@ai_chat_bp.route("/chat")
 def chat():
-    print(request.args.get("question"))
-    question = request.args.get("question")
-    response = ollama.chat(
-        model=desiredModel, messages=[{"role": "user", "content": question}]
-    )
+    try:
+        print(request.args.get("question"))
+        question = request.args.get("question")
+        response = ollama.chat(
+            model=desiredModel, messages=[{"role": "user", "content": question}]
+        )
+        return response["message"]["content"]
+    except Exception as e:
+        return jsonify({"error": f"Error {e}"}), 503
 
-    return response["message"]["content"]
 
-
+app.register_blueprint(ai_chat_bp)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2222)
+    app.run(debug=True, host="0.0.0.0", port=2222)
